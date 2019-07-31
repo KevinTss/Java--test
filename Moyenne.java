f3f6f5d55e2d6a95f932d6249bcdb3f92f77a125
Kevin Tassi <kevin.tassi@gmail.com>
2019-07-31 11:35:13 +0200

    Add Moyenne

---
+++ Moyenne.java
@@ -0,0 +1,14 @@
+public class Moyenne {
+
+  public static void moyenne(int x, int y) {
+    float moyenne = (x + y) / 2;
+
+    System.out.println("La moyenne = " + moyenne); 
+  }
+
+  public static void main(String[] args) {
+
+    moyenne(4, 8);
+
+  }
+}
